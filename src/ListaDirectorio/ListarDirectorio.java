70dd103171caf7edb3346e9070f5a83ffb793bb1
agustina_heredia <cuadernomenstrual@gmail.com>
2023-01-21 09:56:29 +0100

    first commit

--- src/ListaDirectorio/ListarDirectorio.java
+++ src/ListaDirectorio/ListarDirectorio.java
@@ -1,22 +1,33 @@
 package ListaDirectorio;
-
 import java.io.File;
 import java.util.Arrays;
+import java.util.Date;
 
 public class ListarDirectorio {
 
-	public static void main(String[] args) {
-		
-		File directorio = new File("/Users/agustinaheredia/Documents");
-		
-		String[] nombre_archivos = directorio.list();
-		
-		Arrays.sort(nombre_archivos);
-		
-		for(int i = 0; i< nombre_archivos.length; i++) {
-			System.out.println(nombre_archivos[i]);
-		}
-		
-	}
+    public static void main(String[] args) {
 
+        File directorio = new File("/Users/agustinaheredia/Documents");
+        listarDirectorio(directorio);
+    }
+
+    public static void listarDirectorio(File directorio) {
+
+        // Obtenemos el listado de archivos y directorios dentro del directorio
+        File[] contenido = directorio.listFiles();
+
+        // Ordenamos el listado alfabeticamente
+        Arrays.sort(contenido);
+
+        for (File archivo : contenido) {
+            // Si es un directorio, llamamos recursivamente al método
+            if (archivo.isDirectory()) {
+                System.out.println("Directorio - " + archivo.getName() + " - " + new Date(archivo.lastModified()));
+                listarDirectorio(archivo);
+            } else {
+                // Si es un archivo, imprimimos su nombre y última fecha de modificación
+                System.out.println("Archivo - " + archivo.getName() + " - " + new Date(archivo.lastModified()));
+            }
+        }
+    }
 }
